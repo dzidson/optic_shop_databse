d429025e5f5296a63cefd068ac9cf97e568c31fa
Paweł Sierant <pawelsierant@gmail.com>
2023-09-04 18:21:45 +0200

    Stworzenie pustej bazy danych.

---
+++ Create_database_optic_shop.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE optic_shop
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Polish_Poland.1250'
+    LC_CTYPE = 'Polish_Poland.1250'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
